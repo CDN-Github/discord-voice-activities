80f8b329ead2eb534dd8599253b24a2e65e516b7
xhyrom <generalkubo@gmail.com>
2022-04-15 18:45:57 +0200

    inject

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 const { Plugin } = require('powercord/entities');
 const { getModule } = require('powercord/webpack');
+const { inject, uninject } = require('powercord/injector');
 const { get } = require('powercord/http');
 
 class VoiceActivities extends Plugin {
+    constructor() {
+        super();
+
+        this.prop = getModule(['getEnabledAppIds'], false);
+    }
+
     async startPlugin() {
         const activities = JSON.parse((await get('https://raw.githubusercontent.com/xHyroM/discord-activities/master/activities.json')).body.toString());
-
-        const prop = await getModule(['getEnabledAppIds']);
         const appIds = activities.map(activity => activity.id);
 
-        Object.defineProperty(prop, 'getEnabledAppIds', {
-            value: () => appIds,
-            writable: true
-        })
+        inject('voice-activities', this.prop.__proto__, 'getEnabledAppIds', () => appIds);
     }
 
     async pluginWillUnload() {
-        const prop = await getModule(['getEnabledAppIds']);
-
-        Object.defineProperty(prop, 'getEnabledAppIds', {
-            value: () => [],
-            writable: true
-        })
+        uninject('voice-activities');
     }
 }
 
